9ebbbacad5a973a63bf01751820366548b3a6196
bleanovaya <lina.otherside666@gmail.com>
2025-08-20 06:47:36 +0300

    fix: fix adaptive

--- fight.js/screen.js
+++ fight.js/screen.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-window.onload = () => {
   const name = localStorage.getItem('playerName') || 'Player';
   const wins = localStorage.getItem('wins') || 0;
   const losses = localStorage.getItem('losses') || 0;
@@ -46,7 +45,6 @@ window.onload = () => {
     document.querySelector(`.${screenClass}`)?.classList.remove('hidden-screen');
     document.querySelector(`.${screenClass}`)?.classList.add('active-screen');
   }
-};
 };
 
 
